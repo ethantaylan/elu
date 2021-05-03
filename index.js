82e35f90b726d6444df63e747fc0207dc955e1b2
Ethan TAYLAN <ethtaylan@gmail.com>
2021-05-03 16:37:33 +0200

    modifs des styles et ajout du boutton menu à gauche

---
+++ index.js
@@ -0,0 +1,5 @@
+function showHide() 
+    let menu = document.querySelector(".menu")
+    let menuBtn = document.getElementById("menuBtn")
+    if (menu.style.height === "0.01px") 
+        menu.style.height = "400px"
