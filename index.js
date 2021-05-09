7d71d7bb1d4f096b758d0e33d5192d61d407f5ae
ethantaylan <ethtaylan@gmail.com>
2021-05-09 13:28:32 +0200

    toggle

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-function showHide() 
-    let menu = document.querySelector(".menu")
-    let menuBtn = document.getElementById("menuBtn")
-    if (menu.style.height === "0.01px") 
-        menu.style.height = "400px"
+function showHide() {
+    let boite = document.getElementById ("boite")
+    let btn = document.getElementById ("btn")
+    if (boite.style.height === "0px") {
+    boite.style.height = "300px"
+    boite.style.visibility ="visible"
+    boite.style.opacity ="1"
+    boite.style.transition = "opacity 0.3s"
+    }
+    else {
+    boite.style.height ="0px"
+    boite.style.visibility = "hidden"
+    boite.style.opacity ="0"
+    boite.style.transition = "opacity 0.3s, height 1s"
+    }
+    }
